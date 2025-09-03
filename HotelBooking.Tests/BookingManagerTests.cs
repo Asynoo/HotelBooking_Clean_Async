0dc8a6d723c065ff3ee400d0b08098a8ec971d2a
Tegridy Code / Asyno <47446626+Asynoo@users.noreply.github.com>
2025-09-03 23:04:33 +0200

    Cut excess and added unit test skeleton

---
+++ HotelBooking.Tests/BookingManagerTests.cs
@@ -0,0 +1,96 @@
+﻿using HotelBooking.Core;
+using Moq;
+using Xunit;
+
+public class BookingManagerTests
+{
+        private readonly Mock<IRepository<Booking>> bookingRepoMock;
+        private readonly Mock<IRepository<Room>> roomRepoMock;
+        private readonly BookingManager bookingManager;
+
+        public BookingManagerTests()
+        {
+            // TODO: Initialize mocks and BookingManager
+        }
+
+        // -------------------------------
+        // CreateBooking tests
+        // -------------------------------
+        [Fact]
+        public async void CreateBooking_ShouldWork_WhenRoomFree()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void CreateBooking_ShouldFail_WhenNoRoomsFree()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void CreateBooking_ShouldHandleInvalidBooking()
+        {
+            // TODO: Implement this test
+        }
+
+        // -------------------------------
+        // FindAvailableRoom tests
+        // -------------------------------
+        [Fact]
+        public async void FindAvailableRoom_ShouldReturnRoomId_WhenRoomFree()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void FindAvailableRoom_ShouldReturnMinusOne_WhenAllRoomsBooked()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void FindAvailableRoom_ShouldThrow_WhenDatesInvalid()
+        {
+            // TODO: Implement this test
+        }
+
+        // -------------------------------
+        // Data-driven test example
+        // -------------------------------
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(2, 5)]
+        [InlineData(5, 5)]
+        public async void FindAvailableRoom_DataDriven_Test(int startOffset, int endOffset)
+        {
+            // TODO: Implement this test
+        }
+
+        // -------------------------------
+        // GetFullyOccupiedDates tests
+        // -------------------------------
+        [Fact]
+        public async void GetFullyOccupiedDates_ShouldBeEmpty_WhenNoBookings()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void GetFullyOccupiedDates_ShouldReturnDates_WhenAllRoomsFull()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void GetFullyOccupiedDates_ShouldBeEmpty_WhenSomeRoomsFree()
+        {
+            // TODO: Implement this test
+        }
+
+        [Fact]
+        public async void GetFullyOccupiedDates_ShouldThrow_WhenStartAfterEnd()
+        {
+            // TODO: Implement this test
+        }
+    }
